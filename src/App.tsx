11011eccff80b87ca03affe5cccfa9ef64cb1306
maximebergerard <maxime.bergerard@gmail.com>
2023-07-12 19:49:56 +0200

    🎉 Git init !

---
+++ src/App.tsx
@@ -0,0 +1,113 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import * as THREE from "three"
+import { useRef, useState, Suspense } from "react"
+import "./App.css"
+
+import { Canvas } from "@react-three/fiber"
+import {
+  useHelper,
+  OrbitControls,
+  GradientTexture,
+  RoundedBox,
+  OrthographicCamera,
+  PerspectiveCamera,
+  PresentationControls,
+} from "@react-three/drei"
+
+import Man from "./Components/Man"
+import Name from "./Components/Name"
+
+const Light = () => {
+  const directionalLight = useRef(new THREE.DirectionalLight())
+  // useHelper(directionalLight, THREE.DirectionalLightHelper, 1, "cyan")
+  return (
+    <>
+      <ambientLight intensity={0.1} />
+      <directionalLight
+        ref={directionalLight}
+        position={[0, 12, 16]}
+        color={"white"}
+        castShadow={true}
+        intensity={1}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-far={50}
+        shadow-camera-left={-20}
+        shadow-camera-right={20}
+        shadow-camera-top={20}
+        shadow-camera-bottom={-20}
+      />
+    </>
+  )
+}
+
+const OCamera = () => {
+  const camera = useRef<THREE.OrthographicCamera>(null!)
+  // useHelper(camera, THREE.CameraHelper)
+  return (
+    <OrthographicCamera
+      makeDefault
+      position={[0, 1, 40]}
+      rotation={[0, 0, 0]}
+      ref={camera}
+      near={0.1}
+      far={100}
+      zoom={40}
+    />
+  )
+}
+
+const PCamera = () => {
+  // const camera = useRef<THREE.PerspectiveCamera>(null!)
+  // useHelper(camera, THREE.CameraHelper)
+  return (
+    <PerspectiveCamera makeDefault position={[0, 0, 40]}>
+      <OrbitControls />
+    </PerspectiveCamera>
+  )
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas shadows>
+        <GradientTexture
+          attach={"background"}
+          stops={[0, 0.25, 0.75, 1]}
+          colors={["#d0bdde", "#eaafc8", "#a88cf5", "#d0bdde"]}
+        />
+        <OCamera />
+
+        <PresentationControls
+          snap
+          global
+          rotation={[0, Math.PI / 4, 0]}
+          polar={[Math.PI / 20, Math.PI / 4]}
+          azimuth={[-Math.PI / 4, Math.PI / 4]}
+          config={{
+            mass: 1,
+            tension: 60,
+            friction: 20,
+          }}
+        >
+          <Name />
+          <RoundedBox
+            args={[32, 30, 1]}
+            radius={0.4}
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -0.5, 0]}
+            receiveShadow={true}
+          >
+            <meshStandardMaterial color={"#a88cf5"}></meshStandardMaterial>
+          </RoundedBox>
+          <Suspense fallback={null}>
+            <Man />
+          </Suspense>
+        </PresentationControls>
+        {/* <PCamera /> */}
+        <Light />
+      </Canvas>
+    </div>
+  )
+}
---
+++ src/Components/Name.tsx
@@ -0,0 +1,146 @@
+import { useSpring, animated } from "@react-spring/three"
+import { useCursor, Text3D } from "@react-three/drei"
+import { useRef, useState } from "react"
+import * as THREE from "three"
+
+const Letters = [
+  {
+    letter: "M",
+    position: new THREE.Vector3(-15, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "A",
+    position: new THREE.Vector3(-12.5, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "X",
+    position: new THREE.Vector3(-10.45, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "I",
+    position: new THREE.Vector3(-8.6, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "M",
+    position: new THREE.Vector3(-7.65, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "E",
+    position: new THREE.Vector3(-5.2, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "B",
+    position: new THREE.Vector3(-2, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "E",
+    position: new THREE.Vector3(-0.3, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "R",
+    position: new THREE.Vector3(1.4, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "G",
+    position: new THREE.Vector3(3.1, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "E",
+    position: new THREE.Vector3(4.9, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "R",
+    position: new THREE.Vector3(6.5, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "A",
+    position: new THREE.Vector3(8.4, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "R",
+    position: new THREE.Vector3(10.35, 0, 0),
+    rotationX: 0,
+  },
+  {
+    letter: "D",
+    position: new THREE.Vector3(12.2, 0, 0),
+    rotationX: 0,
+  },
+]
+
+interface LetterProps {
+  letter: string
+  position: THREE.Vector3
+  rotationX: number
+}
+
+const Letter = ({ letter, position, rotationX }: LetterProps) => {
+  const fontSize = 2
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const ref = useRef<THREE.Mesh>(null!)
+
+  const [hovered, set] = useState(false)
+  useCursor(hovered /*'pointer', 'auto'*/)
+
+  const { rotation } = useSpring({
+    rotation: hovered ? [-Math.PI / 2, 0, 0] : [0, 0, 0],
+    config: {
+      tension: 80,
+    },
+  })
+
+  return (
+    <animated.group
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      rotation={rotation}
+      onPointerOver={() => set(true)}
+      onPointerOut={() => setTimeout(() => set(false), 4000)}
+    >
+      <Text3D
+        ref={ref}
+        font={"./fonts/Bree Serif_Regular.json"}
+        position={position}
+        rotation={[rotationX, 0, 0]}
+        size={fontSize}
+        bevelEnabled
+        bevelSize={0.05}
+        castShadow
+      >
+        {letter}
+        {/* <meshNormalMaterial /> */}
+        <meshStandardMaterial color={"#ffffff"} />
+      </Text3D>
+    </animated.group>
+  )
+}
+
+const Name = () => {
+  return (
+    <group position={[0.5, 0, -10]}>
+      {Letters.map((letter, i) => (
+        <Letter
+          key={i}
+          letter={letter.letter}
+          position={letter.position}
+          rotationX={letter.rotationX}
+        />
+      ))}
+    </group>
+  )
+}
+
+export default Name
