890166eb4a51b5d687b581ca561bea43bcb82181
maximebergerard <maxime.bergerard@hetic.net>
2023-12-19 14:49:44 +0100

    ✨ Add mailbox 3d obj, todo : add email animation

--- src/Components/MailBox/MailBoxScene.tsx
+++ src/Components/MailBox/MailBoxScene.tsx
@@ -1,9 +1,17 @@
+import { useFBX } from "@react-three/drei"
 import Grass from "./Grass"
 
 const MailBoxScene = () => {
+  const fbx = useFBX("./3dmodels/MailBox/mailbox.fbx")
   return (
     <>
       <Grass />
+      <primitive
+        object={fbx}
+        scale={0.04}
+        position={[14, 1.8, -1.6]}
+        rotation={[0, (-7 * Math.PI) / 9, 0]}
+      />
     </>
   )
 }
